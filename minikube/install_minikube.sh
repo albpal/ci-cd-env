cdd2ae323bbe771ba3d0cffa51f83cbc3716b065
Albert Palau <anorkbcn@gmail.com>
2019-08-11 09:02:36 +0200

    Remove X forwarding stuff
    It's a better approach install kubectl in remote clients and access to the cluster through it. Furthermore, kubectl can proxing requests to access k8s dashboard as if it was local, among others

--- minikube/install_minikube.sh
+++ minikube/install_minikube.sh
@@ -15,7 +15,7 @@ gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
-yum install kubectl xdg-utils xauth xclock -y
+yum install kubectl -y
 systemctl stop kubepods-burstable.slice
 sysctl -w net.bridge.bridge-nf-call-iptables=1
 $(dirname $0)/start_minikube.sh
